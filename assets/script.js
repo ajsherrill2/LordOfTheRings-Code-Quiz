e69d7cbf5a2f826f627bc39601d0b6079b1e90fc
GH0ST <adamjamessherrill@gmail.com>
2022-08-21 23:59:00 -0500

    added styles and created a hide element function within the java

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,6 @@ timerEl.textContent = 'Time: ' + '';
 function countDown() {
     var timeLeft = 100;
 
-
     var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
             timerEl.textContent = 'Time: ' + timeLeft;
@@ -20,6 +19,13 @@ function countDown() {
             clearInterval(timeInterval);
         }
     }, 1000);
+
+    var hide = document.getElementById("intro");
+    if (hide.style.display === "none") {
+        hide.style.display = "block";
+    } else {
+        hide.style.display = "none";
+    }
 };
 
 startQuiz.addEventListener('click', function() {
